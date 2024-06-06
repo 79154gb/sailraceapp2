ed45e7fbb7fba9613d0980289137879960dc2a10
79154gb <79154@proton.me>
2024-06-06 19:59:17 +0100

    working app

--- screens/RaceOverviewScreen.js
+++ screens/RaceOverviewScreen.js
@@ -1,243 +1,333 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import MapView, {Marker} from 'react-native-maps';
+import axios from 'axios';
 import Icon from 'react-native-vector-icons/Entypo';
+import Icon1 from 'react-native-vector-icons/Feather';
 
-const RaceOverviewScreen = ({route}) => {
-  const {
-    raceStartTime,
-    windSpeed,
-    windDirection,
-    tideSpeed,
-    tideDirection,
-    passToOverview,
-  } = route.params;
+const markerTypes = [
+  {label: 'Start Mark 1', value: 'start1', color: 'blue'},
+  {label: 'Start Mark 2', value: 'start2', color: 'cyan'},
+  {label: 'Windward Mark', value: 'windward', color: 'red'},
+  {label: 'Leeward Mark', value: 'leeward', color: 'green'},
+  {label: 'Reach Mark', value: 'reach', color: 'yellow'},
+];
 
-  const [timer, setTimer] = useState('10:00');
-  const intervalRef = useRef(null);
-  const [windDirectionRotation, setWindDirectionRotation] = useState(0);
-
-  useEffect(() => {
-    if (passToOverview && raceStartTime) {
-      const [hours, mins] = raceStartTime.split(':').map(Number);
-      const raceTime = new Date();
-      raceTime.setHours(hours);
-      raceTime.setMinutes(mins);
-      raceTime.setSeconds(0);
-
-      const updateTimer = () => {
-        const currentTime = new Date();
-        const timeDifference =
-          raceTime.getTime() - currentTime.getTime() - 600000; // 10 minutes before race start
-        if (timeDifference <= 0) {
-          clearInterval(intervalRef.current);
-          setTimer('00:00');
-        } else {
-          const minutes = Math.floor(timeDifference / 60000);
-          const seconds = Math.floor((timeDifference % 60000) / 1000);
-          setTimer(
-            `${minutes.toString().padStart(2, '0')}:${seconds
-              .toString()
-              .padStart(2, '0')}`,
-          );
-        }
-      };
-
-      intervalRef.current = setInterval(updateTimer, 1000);
-
-      return () => clearInterval(intervalRef.current);
-    }
-  }, [raceStartTime, passToOverview]);
+const RaceOverviewScreen = () => {
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarkerType, setSelectedMarkerType] = useState(markerTypes[0]);
+  const [showModal, setShowModal] = useState(false);
+  const [windArrows, setWindArrows] = useState([]);
+  const [storedWindData, setStoredWindData] = useState(null); // State to store wind data
+  const [windSpeed, setWindSpeed] = useState(null); // State to store wind speed
+  const [tideInfo, setTideInfo] = useState(null); // State to store tide information
+  const [date, setDate] = useState(new Date()); // State to store selected date
+  const [time, setTime] = useState(new Date()); // State to store selected time
+  const [showDatePicker, setShowDatePicker] = useState(false); // State to show/hide date picker
+  const [showTimePicker, setShowTimePicker] = useState(false); // State to show/hide time picker
+  const mapRef = useRef(null);
 
   useEffect(() => {
-    if (windDirection !== undefined) {
-      const rotation = windDirection % 360; // Ensure rotation is within 0 to 359 degrees
-      setWindDirectionRotation(rotation);
-    }
-  }, [windDirection]);
+    const fetchWindData = async () => {
+      const location = 'San%20Francisco%20bay'; // Replace with dynamic location if needed
+      const startDate = formatDate(date); // Format selected date
+      const endDate = formatDate(
+        new Date(date.getTime() + 24 * 60 * 60 * 1000),
+      ); // Add 24 hours to the start date
+      const apiKey = 'XJP7QFLSXN7JSLWWKHBPPLEHC'; // Replace with your actual API key
 
-  const startTimer = () => {
-    clearInterval(intervalRef.current);
-    setTimer('10:00');
-    intervalRef.current = setInterval(() => {
-      setTimer(prevTimer => {
-        const [prevMinutes, prevSeconds] = prevTimer.split(':').map(Number);
-        if (prevMinutes === 0 && prevSeconds === 0) {
-          clearInterval(intervalRef.current);
-          return '00:00';
-        }
-        const totalSeconds = prevMinutes * 60 + prevSeconds - 1;
-        const newMinutes = Math.floor(totalSeconds / 60);
-        const newSeconds = totalSeconds % 60;
-        return `${newMinutes.toString().padStart(2, '0')}:${newSeconds
-          .toString()
-          .padStart(2, '0')}`;
-      });
-    }, 1000);
+      const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${startDate}/${endDate}?key=${apiKey}`;
+
+      try {
+        const response = await axios.get(apiUrl);
+        const data = response.data.days[0].hours; // Adjust according to actual response structure
+        console.log('API Response Data:', data); // Add this line to inspect the structure of the data
+
+        const arrows = data.map((hour, index) => ({
+          latitude: 52.52 + index * 0.01, // Example latitude spread
+          longitude: 13.41 + index * 0.01, // Example longitude spread
+          windSpeed: hour.windSpeed,
+          windDirection: hour.windDirection,
+        }));
+
+        setWindArrows(arrows);
+        setStoredWindData(arrows); // Store the wind data
+        setWindSpeed(data[0].windSpeed); // Set wind speed
+        setTideInfo(data[0].tideInfo); // Set tide information
+      } catch (error) {
+        console.error('Error fetching wind data:', error);
+      }
+    };
+
+    // Fetch wind data to display wind arrows on the map
+    fetchWindData();
+  }, [date]);
+
+  const formatDate = formattedDate => {
+    const year = formattedDate.getFullYear();
+    const month = (formattedDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = formattedDate.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
   };
+
+  const handleMapPress = event => {
+    const newMarker = {
+      coordinate: event.nativeEvent.coordinate,
+      key: `${markers.length}`,
+      type: selectedMarkerType,
+      color: selectedMarkerType.color,
+    };
+    setMarkers([...markers, newMarker]);
+  };
+
+  const handleMarkerDragEnd = (event, index) => {
+    const updatedMarkers = [...markers];
+    updatedMarkers[index].coordinate = event.nativeEvent.coordinate;
+    setMarkers(updatedMarkers);
+  };
+
+  const zoomIn = () => {
+    mapRef.current.animateCamera({zoom: 1}, {duration: 1000});
+  };
+
+  const zoomOut = () => {
+    mapRef.current.animateCamera({zoom: -1}, {duration: 1000});
+  };
+
+  const renderModalItem = ({item}) => (
+    <TouchableOpacity
+      style={styles.modalItem}
+      onPress={() => {
+        setSelectedMarkerType(item);
+        setShowModal(false);
+      }}>
+      <Text>{item.label}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      <View style={styles.compassContainer}>
-        <Icon
-          name="arrow-up"
-          size={30}
-          color="black"
-          style={{transform: [{rotate: `${windDirectionRotation}deg`}]}}
-        />
-      </View>
-      <View style={styles.legendContainer}>
-        {['Windward', 'Leeward', 'Starting Line'].map((label, index) => (
-          <Text key={index} style={styles.legendItem}>
-            {label}
-          </Text>
-        ))}
-      </View>
-      <View style={styles.raceCourseGrid}>
-        <View style={styles.water} />
-        {[...Array(10)].map((_, index) => (
-          <View
-            key={index}
-            style={[
-              styles.gridLine,
-              styles.verticalGridLine,
-              {left: `${(100 / 10) * (index + 1)}%`},
-            ]}
+      {/* MapView */}
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        onPress={handleMapPress}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        zoomEnabled={true}>
+        {/* Display markers */}
+        {markers.map((marker, index) => (
+          <Marker
+            key={marker.key}
+            coordinate={marker.coordinate}
+            pinColor={marker.color}
+            draggable
+            onDragEnd={event => handleMarkerDragEnd(event, index)}
+            title={`Marker ${marker.key}`}
+            description={marker.type.label}
           />
         ))}
-        {[...Array(10)].map((_, index) => (
-          <View
+
+        {/* Display wind arrows */}
+        {windArrows.map((arrow, index) => (
+          <Marker
             key={index}
-            style={[
-              styles.gridLine,
-              styles.horizontalGridLine,
-              {top: `${(100 / 10) * (index + 1)}%`},
-            ]}
-          />
+            coordinate={{latitude: arrow.latitude, longitude: arrow.longitude}}
+            title={`Wind: ${arrow.windSpeed} knots`}
+            description={`Direction: ${arrow.windDirection}°`}>
+            <View style={{transform: [{rotate: `${arrow.windDirection}deg`}]}}>
+              <Icon name="arrow-up" size={20} color="white" />
+            </View>
+          </Marker>
         ))}
-        <View
-          style={[styles.marker, styles.markerStart, {top: '20%', left: '50%'}]}
-        />
-        <View
-          style={[
-            styles.marker,
-            styles.markerWindward,
-            {top: '10%', left: '50%'},
-          ]}
-        />
-        <View
-          style={[styles.marker, styles.markerReach, {top: '50%', left: '70%'}]}
-        />
-        <View
-          style={[
-            styles.marker,
-            styles.markerLeeward,
-            {top: '90%', left: '20%'},
-          ]}
-        />
-      </View>
-      <View style={styles.weatherInfo}>
-        <View style={styles.weatherItem}>
-          <Text style={styles.weatherText}>
-            Wind: {windSpeed ? windSpeed.toFixed(2) : 'N/A'} knots{' '}
-            {windDirection ? windDirection.toFixed(2) : 'N/A'}°
-          </Text>
+      </MapView>
+
+      {/* Information panel */}
+      <View style={styles.infoContainer}>
+        {/* Date Picker */}
+        <TouchableOpacity
+          style={styles.infoItem}
+          onPress={() => setShowDatePicker(true)}>
+          <Icon name="calendar" size={20} color="black" />
+          <Text style={styles.infoText}>Date: {formatDate(date)}</Text>
+        </TouchableOpacity>
+
+        {/* Time Picker */}
+        <TouchableOpacity
+          style={styles.infoItem}
+          onPress={() => setShowTimePicker(true)}>
+          <Icon name="clock" size={20} color="black" />
+          <Text style={styles.infoText}>Time: {time.toLocaleTimeString()}</Text>
+        </TouchableOpacity>
+
+        {/* Wind */}
+        <View style={styles.infoItem}>
+          <Icon1 name="wind" size={20} color="black" />
+          <Text style={styles.infoText}>Wind: {windSpeed} knots</Text>
         </View>
-        <View style={styles.weatherItem}>
-          <Icon name="air" size={25} color="black" />
-          <Text style={styles.weatherText}>
-            Tide: {tideSpeed ? tideSpeed.toFixed(2) : 'N/A'} knots{' '}
-            {tideDirection ? tideDirection.toFixed(2) : 'N/A'}°
-          </Text>
+
+        {/* Tide */}
+        <View style={styles.infoItem}>
+          <Icon name="water" size={20} color="black" />
+          <Text style={styles.infoText}>Tide: {tideInfo}</Text>
         </View>
-        <Text style={styles.timer}>Timer: {timer}</Text>
-        <Button title="Start Timer" onPress={startTimer} />
+
+        {/* Marker type selector */}
+        <TouchableOpacity
+          style={styles.picker}
+          onPress={() => setShowModal(true)}>
+          <Text>{selectedMarkerType.label}</Text>
+        </TouchableOpacity>
       </View>
+
+      {/* Button to clear markers */}
+      <View style={styles.buttonContainer}>
+        <Button title="Clear Markers" onPress={() => setMarkers([])} />
+      </View>
+
+      {/* Zoom In/Out Buttons */}
+      <View style={styles.zoomButtons}>
+        <TouchableOpacity onPress={zoomIn} style={styles.zoomButton}>
+          <Text style={styles.zoomText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={zoomOut} style={styles.zoomButton}>
+          <Text style={styles.zoomText}>-</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Date Picker Modal */}
+      {showDatePicker && (
+        <DateTimePicker
+          value={date}
+          mode="date"
+          display="default"
+          onChange={(event, selectedDate) => {
+            setShowDatePicker(false);
+            if (selectedDate) {
+              setDate(selectedDate);
+            }
+          }}
+        />
+      )}
+
+      {/* Time Picker Modal */}
+      {showTimePicker && (
+        <DateTimePicker
+          value={time}
+          mode="time"
+          display="default"
+          onChange={(event, selectedTime) => {
+            setShowTimePicker(false);
+            if (selectedTime) {
+              setTime(selectedTime);
+            }
+          }}
+        />
+      )}
+
+      {/* Marker type modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => setShowModal(false)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <FlatList
+              data={markerTypes}
+              renderItem={renderModalItem}
+              keyExtractor={item => item.value}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  infoContainer: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+  infoItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  infoText: {
+    marginLeft: 5,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+  },
+  zoomButtons: {
+    position: 'absolute',
+    bottom: 100,
+    right: 20,
+    flexDirection: 'column',
+  },
+  zoomButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    borderRadius: 5,
+    elevation: 2,
+  },
+  zoomText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFF',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-  compassContainer: {
-    position: 'absolute',
-    top: 10,
-    left: 10,
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
   },
-  legendContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '100%',
-    marginTop: 20,
-  },
-  legendItem: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  raceCourseGrid: {
-    width: 300,
-    height: 300,
-    borderWidth: 1,
-    borderColor: 'black',
-    position: 'relative',
-    marginTop: 20,
-  },
-  gridLine: {
-    position: 'absolute',
-    borderColor: 'black',
-    borderWidth: 0.5,
-  },
-  horizontalGridLine: {
-    width: '100%',
-    height: 0,
-  },
-  verticalGridLine: {
-    height: '100%',
-    width: 0,
-  },
-  water: {
-    flex: 1,
-    backgroundColor: 'lightblue',
-  },
-  marker: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    position: 'absolute',
-  },
-  markerWindward: {
-    backgroundColor: 'red',
-  },
-  markerReach: {
-    backgroundColor: 'yellow',
-  },
-  markerLeeward: {
-    backgroundColor: 'green',
-  },
-  markerStart: {
-    backgroundColor: 'blue',
-  },
-  weatherInfo: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
-  weatherItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 5,
-  },
-  weatherText: {
-    fontSize: 16,
-    marginLeft: 5,
-  },
-  timer: {
-    fontSize: 16,
-    fontWeight: 'bold',
+  modalItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
   },
 });
 
