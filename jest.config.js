772a6a3075f04f10937538a2ad345d1c5d9fdde4
gerard <sailracerapp@gmail.com>
2024-08-21 20:19:17 +0100

    Fixed to jest.config.js and testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  testMatch: ['**/__tests__/**/*.test.tsx', '**/?(*.)+(spec|test).ts?(x)'], // Look for .test.tsx and .spec.tsx files
+  testPathIgnorePatterns: ['/node_modules/', '/jest.config.js/'], // Ignore config file itself
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest', // Handle JS and TS files
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js', // Mock image files
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
