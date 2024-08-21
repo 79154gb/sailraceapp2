dfc95e35b58d2e42f59d2b2710d8988631665105
gerard <sailracerapp@gmail.com>
2024-08-21 19:43:38 +0100

    babel update as result of bitrise fail

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
+    'module:metro-react-native-babel-preset', // Use the Metro preset for React Native
   ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+      ],
+    },
+  },
 };
